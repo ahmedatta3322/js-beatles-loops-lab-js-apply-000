ef2a47e71ecc99a1ac2b01cdb585d934d74e747b
ahmed atta <ahmedatta3322@gmail.com>
2018-11-17 16:10:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(x,y){
+  var r = []
+  for(let i=0; i < x.length ; i++){
+    var s = x[i] + " plays " + y[i]
+    r.push(s)
+  }
+  return r
+}
+function johnLennonFacts(facts){
+  let x = 0 
+  while (x<facts.length){
+    facts[x] = facts[x] +"!!!"
+    x = x + 1 
+  }
+  return facts
+  
+}
+function iLoveTheBeatles(y){
+  var x = []
+  do {x.push("I love the Beatles!")}
+  while (y<15){
+    y = y + 1 ;
+  }
+  return x 
+}
